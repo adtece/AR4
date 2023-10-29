69f4965e7d8e398ea9654ea45c68725de8b032ba
adtece <146814043+adtece@users.noreply.github.com>
2023-10-29 17:02:15 +0100

    Add files via upload

---
+++ ej8.php
@@ -0,0 +1,26 @@
+<?php
+
+$arr = [
+    [1, 5, 8, 5],
+    [7, 3, 2, 4],
+    [1, 6, 2, 4],
+];
+
+echo "Array original: <br>";
+foreach ($arr as $fila) {
+    echo implode(",", $fila) . "<br>";
+}
+
+echo "Paso de filas a columnas: <br>";
+
+$arrTrans = [];
+for ($i = 0; $i < count($arr[0]); $i++) {
+    array_push($arrTrans, []);
+    for ($j = 0; $j < count($arr); $j++) {
+        array_push($arrTrans[$i], $arr[$j][$i]);
+    }
+}
+
+foreach ($arrTrans as $fila) {
+    echo implode(",", $fila) . "<br>";
+}
